0b3427cd762a44e4f72df6c260baf66627d97067
shatabdi <mshatabdii03@gmail.com>
2026-02-10 10:50:35 +0530

    Initial commit: setup backend with MongoDB and Redis

---
+++ backend/user/src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from 'dotenv';
+import connectDb from "./config/db.js";
+import { createClient } from "redis";
+import userRoutes from './routes/user.js';
+
+
+dotenv.config();
+
+/* -------- ENV VALIDATION -------- */
+const port = process.env.PORT;
+const REDIS_URL = process.env.REDIS_URL;
+
+if (!port) {
+  throw new Error("PORT is not defined in .env");
+}
+
+if (!REDIS_URL) {
+  throw new Error("REDIS_URL is not defined in .env");
+}
+/* -------------------------------- */
+
+connectDb();
+
+export const redisClient= createClient({
+    url: REDIS_URL,
+});
+
+redisClient
+.connect()
+.then(() =>console.log("Connected to redis"))
+.catch((err)=>{
+    console.error("Redis connection failed", err);
+    process.exit(1);
+});
+
+const app = express();
+
+app.use("api/v1",userRoutes);
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
